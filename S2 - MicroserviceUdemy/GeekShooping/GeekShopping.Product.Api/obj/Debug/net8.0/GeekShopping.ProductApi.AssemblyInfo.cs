30b623551d221b21384fc6067367ec2a129c7138
Viktor Silveira <viktorsilveira91@gmail.com>
2024-05-22 18:22:35 -0300

    bkp

--- S2 - MicroserviceUdemy/GeekShooping/GeekShopping.Product.Api/obj/Debug/net8.0/GeekShopping.ProductApi.AssemblyInfo.cs
+++ S2 - MicroserviceUdemy/GeekShooping/GeekShopping.Product.Api/obj/Debug/net8.0/GeekShopping.ProductApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeekShopping.ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbcdd77a615391f79ee2366fedf29a305d7bb22e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d04708b189de34edd0b7d0f901574d719b48f2d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeekShopping.ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeekShopping.ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
