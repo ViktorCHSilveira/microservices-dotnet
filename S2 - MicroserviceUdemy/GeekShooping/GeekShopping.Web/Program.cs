682c6d5941efc13741b910574f4c90c9f570b6e2
Viktor Silveira <viktorsilveira91@gmail.com>
2024-05-26 14:28:41 -0300

    iniciando a pagina de web

--- S2 - MicroserviceUdemy/GeekShooping/GeekShopping.Web/Program.cs
+++ S2 - MicroserviceUdemy/GeekShooping/GeekShopping.Web/Program.cs
@@ -1,6 +1,13 @@
+using GeekShopping.web.Services;
+using GeekShopping.web.Services.ISercies;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var Services = builder.Services;
+var Configuration = builder.Configuration;
+
 // Add services to the container.
+builder.Services.AddHttpClient<IProductService, ProductService>(c => c.BaseAddress = new Uri(Configuration["serviceUrls:ProductAPI"]));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
---
+++ S2 - MicroserviceUdemy/GeekShooping/GeekShopping.Web/Services/ISercies/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using GeekShopping.web.Models;
+
+namespace GeekShopping.web.Services.ISercies {
+    public interface IProductService {
+
+        Task<IEnumerable<ProductModel>> FindAllProducts();
+
+        Task<ProductModel> FindProductById(long id);
+        Task<ProductModel> CreateProduct(ProductModel product);
+        Task<ProductModel> UpdateProduct(ProductModel product);
+        Task<bool> DeleteyId(long id);
+    }
+}
